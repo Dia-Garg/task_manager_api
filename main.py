951d01ada0f6566da26ffa98e9dac5feebeb285c
Dia Garg <darjdiagarg@gmail.com>
2026-03-16 22:37:24 +0530

    Add POST and DELETE endpoints to Task Manager API

--- main.py
+++ main.py
@@ -11,3 +11,23 @@ def home():
 @app.get("/tasks")
 def get_tasks():
     return tasks
+
+from pydantic import BaseModel
+
+class Task(BaseModel):
+    title: str
+
+@app.post("/tasks")
+def create_task(task: Task):
+    new_task={"id": len(tasks)+1, "title": task.title, "done": False}
+
+    task.append(new_task)
+    return new_task
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"]==task_id:
+            tasks.remove(task)
+            return {"message": "Task deleted"}
+    return{"error": "Task not found"}
